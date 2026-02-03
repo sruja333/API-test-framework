9809543985fac2107cc3d3605f5607c986f06e04
Sruja <sruja357@gmail.com>
2026-02-04 00:44:45 +0530

    test users

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,15 +1,9 @@
-import requests
 import pytest
 
 @pytest.mark.regression
 @pytest.mark.parametrize("name", ["Alice", "Bob"])
-def test_create_user(config, name):
-    payload = {"name": name}
-    
-    response = requests.post(
-        f"{config['base_url']}/users",
-        json=payload
-    )
+def test_create_user(client, name):
+    response = client.post("/users", json={"name": name})
 
     assert response.status_code == 200
     assert response.json()["name"] == name
