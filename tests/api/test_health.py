5794e21f994348ef16dd37667abde9524742a26d
Sruja <sruja357@gmail.com>
2026-02-04 00:43:55 +0530

    test health

--- tests/api/test_health.py
+++ tests/api/test_health.py
@@ -1,9 +1,8 @@
-import requests
 import pytest
 
 @pytest.mark.smoke
-def test_health_check(config):
-    response = requests.get(f"{config['base_url']}/health")
-    
+def test_health_check(client):
+    response = client.get("/health")
+
     assert response.status_code == 200
     assert response.json()["status"] == "ok"
