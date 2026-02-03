76f0ee9c253223e5b58bbe4b248c81f38bdef543
Sruja <sruja357@gmail.com>
2026-02-04 00:44:23 +0530

    test negative cases

--- tests/api/test_negative_cases.py
+++ tests/api/test_negative_cases.py
@@ -1,10 +1,4 @@
-import requests
-import pytest
-
-def test_create_user_without_name(config):
-    response = requests.post(
-        f"{config['base_url']}/users",
-        json={}
-    )
+def test_create_user_without_name(client):
+    response = client.post("/users", json={})
 
     assert response.status_code == 400
